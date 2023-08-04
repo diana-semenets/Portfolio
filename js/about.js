d00f6b79413aca57734559345b8f80d3d81431a1
diana-semenets <110393311+diana-semenets@users.noreply.github.com>
2023-08-04 19:40:16 +0300

    Add files via upload

--- js/about.js
+++ js/about.js
@@ -9,4 +9,29 @@ tl.fromTo('.about-vacancy', {
     x:0,
     opacity:1,
     duration:1.2,
-})
+}).fromTo('.about-name', {
+    y:50,
+    opacity:0,
+},
+{
+    y:0,
+    opacity:1,
+    duration:1.2,
+},0.8,
+).fromTo('.main__btn', {
+    y:50,
+    opacity:0,
+}, {
+    y:0,
+    opacity:1,
+    duration:1.2,
+}).fromTo('.navbar-nav li', {
+    y:50,
+    opacity:0,
+    
+}, {
+    y:0,
+    opacity:1,
+    duration:1.2,
+    stagger:0.2,
+},1,)
